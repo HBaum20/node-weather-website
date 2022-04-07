4005d3423cd62e1d522f26f3d4bcefe15dca5130
Harry Baum <harry.baum@itv.com>
2022-04-07 20:20:41 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
